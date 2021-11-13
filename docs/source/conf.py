07fdb667364baa42d2527eb55355edd6c4b8246a
Mike Nakos <mnakos@terracom.gr>
2021-11-13 12:01:00 +0200

    v15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,8 +43,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_material'
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
